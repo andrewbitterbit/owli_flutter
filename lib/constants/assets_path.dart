9245a72108704810b0a6a0859ccdcfacda8375ac
Andrew <andrew@bitterbit.tech>
2022-09-01 13:51:36 +0000

    first commit

--- lib/constants/assets_path.dart
+++ lib/constants/assets_path.dart
@@ -1,9 +1,8 @@
 abstract class ThingsboardImage {
-  static final thingsBoardWithTitle =
-      'assets/images/thingsboard_with_title.svg';
-  static final thingsboard = 'assets/images/thingsboard.svg';
+  static final thingsBoardWithTitle = 'assets/images/owli-long.svg';
+  static final thingsboard = 'assets/images/owli.svg';
   static final thingsboardOuter = 'assets/images/thingsboard_outer.svg';
-  static final thingsboardCenter = 'assets/images/thingsboard_center.svg';
+  static final thingsboardCenter = 'assets/images/owli-long.svg';
   static final dashboardPlaceholder = 'assets/images/dashboard-placeholder.svg';
   static final deviceProfilePlaceholder =
       'assets/images/device-profile-placeholder.svg';
